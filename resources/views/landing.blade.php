195f1a8f22f11a6cd3a1f6fca4b238dd06ce5ff9
inziano <inzeeano@gmail.com>
2019-11-28 09:59:56 +0300

    Changes

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>AFF | African Forest Forum </title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <!-- <link rel="stylesheet" href="{{asset('css/app.css')}}"> -->
     <link rel="stylesheet" href="{{secure_asset('css/app.css')}}">
 </head>
 <body>
