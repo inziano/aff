c4af2070ded26249b33e012cb2e65956a68e7d0e
inziano <inzeeano@gmail.com>
2020-01-06 08:34:29 +0300

    Secure asset

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,8 +7,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>AFF | African Forest Forum </title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <link href="https://cdn.quilljs.com/1.2.6/quill.snow.css" rel="stylesheet">
-    <!-- <link rel="stylesheet" href="{{secure_asset('css/app.css')}}"> -->
+    <!-- <link href="https://cdn.quilljs.com/1.2.6/quill.snow.css" rel="stylesheet"> -->
+    <link rel="stylesheet" href="{{secure_asset('css/app.css')}}">
 </head>
 <body>
     <div id="app" class="h-full w-full" style="height: 100vh;">
